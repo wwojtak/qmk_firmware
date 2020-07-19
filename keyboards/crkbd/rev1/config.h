995a1eeadeb1df17d524649c6394cded2e099192
Drashna Jael're <drashna@live.com>
2020-07-18 23:48:17 -0700

    [MERGE] Convert Corne Keyboard to Split Common (6001)

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -36,6 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 // #define MATRIX_COL_PINS { B2, B3, B1, F7, F6, F5, F4 } //uncomment this line and comment line above if you need to reverse left-to-right key order
 
+#define DIODE_DIRECTION COL2ROW
+
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
@@ -50,16 +52,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 //#define LOCKING_RESYNC_ENABLE
 
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D2
+
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
 #ifdef RGBLIGHT_ENABLE
-#define RGBLED_NUM 12    // Number of LEDs
+#    define RGBLED_NUM 54  // Number of LEDs
+#    define RGBLED_SPLIT { 27, 27 }
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
-#define RGBLED_NUM 54    // Number of LEDs
-#define DRIVER_LED_TOTAL RGBLED_NUM
+#    define RGBLED_NUM 54  // Number of LEDs
+#    define DRIVER_LED_TOTAL RGBLED_NUM
+#    define RGB_MATRIX_SPLIT { 27, 27 }
+#endif
+
+#ifdef SSD1306OLED
+#    define USE_I2C
 #endif
 
 /*
