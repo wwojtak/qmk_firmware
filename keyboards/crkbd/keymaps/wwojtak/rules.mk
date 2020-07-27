31c674947036c1aa03875e6298c8a9aa2bb90cd6
Wojciech Wojtak <wojciech.wojtak@maersk.com>
2020-07-27 12:22:24 +0100

    add my stuff back

---
+++ keyboards/crkbd/keymaps/wwojtak/rules.mk
@@ -0,0 +1,12 @@
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = WS2812
+NKRO_ENABLE        = yes
+OLED_DRIVER_ENABLE = yes
+TAP_DANCE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+
+BOOTMAGIC_ENABLE           = no  # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE            = no  # Mouse keys(+4700)
+CONSOLE_ENABLE             = no  # Console for debug(+400)
+COMMAND_ENABLE             = no  # Commands for debug and configuration
+RAW_ENABLE = yes
