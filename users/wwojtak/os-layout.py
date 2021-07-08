a6a24e3add784f32b99377ce7e17be497497d898
Wojciech Wojtak <wojciech.wojtak@maersk.com>
2021-07-08 12:15:49 +0100

    loop for os-update script

--- users/wwojtak/os-layout.py
+++ users/wwojtak/os-layout.py
@@ -1,7 +1,6 @@
 import hid
 import platform
-from time import sleep
-
+from time import sleep, time
 
 def tobyte(data):
     if type(data) is bytes:
@@ -17,43 +16,61 @@ def tobytes(data):
     return out
 
 
-usage_page = int.from_bytes(b'\xFF\x60', 'big')
-usage_id = int.from_bytes(b'\x61', 'big')
+def search():
+    usage_page = int.from_bytes(b'\xFF\x60', 'big')
+    usage_id = int.from_bytes(b'\x61', 'big')
 
-system = platform.system()
-release = platform.release()
+    devices = hid.enumerate()
+    for device in devices:
+        if (device['manufacturer_string'] == "wwojtak"
+                and device['usage_page'] == usage_page
+                and device['usage'] == usage_id):
+            device = hid.Device(path=device['path'])
+            return True, device
+    return False, None
 
-ctrlgui_default = tobytes([0, 1, 0])
-ctrlgui_swapped = tobytes([0, 1, 1])
 
-devices = hid.enumerate()
-for device in devices:
-    if (device['manufacturer_string'] == "wwojtak"
-            and device['usage_page'] == usage_page
-            and device['usage'] == usage_id):
-        device = hid.Device(path=device['path'])
-        print('connected to {} by {}'.format(
-            device.product, device.manufacturer))
-        print('running on {} {}'.format(system, release))
-        sleep(0.05)
-        if system == "Darwin":
-            data = ctrlgui_swapped
-            print('sent ', int.from_bytes(data, 'big'))
+def update(device):
+    ctrlgui_default = tobytes([0, 1, 0])
+    ctrlgui_swapped = tobytes([0, 1, 1])
+
+    system = platform.system()
+    release = platform.release()
+
+    epoch_time = int(time())
+
+    print('[{}] connected to {} by {}'.format(
+        epoch_time, device.product, device.manufacturer))
+    print('running on {} {}'.format(system, release))
+    sleep(0.05)
+    if system == "Darwin":
+        data = ctrlgui_swapped
+        print('sent ', int.from_bytes(data, 'big'))
+    else:
+        data = ctrlgui_default
+        print('sent ', int.from_bytes(data, 'big'))
+    device.write(data)
+    sleep(0.05)
+    ctrlgui = int.from_bytes(device.read(2), 'big')
+    if ctrlgui == 0:
+        print('{} using default ctrlgui'.format(device.product))
+    else:
+        print('received ', ctrlgui)
+        print('{} using swapped ctrlgui'.format(device.product))
+    device.close()
+    return True
+
+
+if __name__ == "__main__":
+    updated = False
+    while True:
+        device = search()
+        if (device[0] and not updated):
+            if (update(device[1])):
+                device[1].close()
+                updated = True
+        elif (device[0]):
+            device[1].close()
         else:
-            data = ctrlgui_default
-            print('sent ', int.from_bytes(data, 'big'))
-        device.write(data)
-        sleep(0.05)
-        ctrlgui = int.from_bytes(device.read(2), 'big')
-        if ctrlgui == 0:
-            print('{} using default ctrlgui'.format(device.product))
-        else:
-            print('received ', ctrlgui)
-            print('{} using swapped ctrlgui'.format(device.product))
-        device.close()
-
-'''    raise HIDException(err)
-hid.HIDException: The device is not connected.
-
-device['product_string'] in ['crkbd', 'preonic', 'bm40hsrgb']
-'''
+            updated = False
+            sleep(2)
